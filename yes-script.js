f31906f77ecfb5847050982860373373ea18179b
Edoardo <rossi.edoardo369@gmail.com>
2026-03-27 19:28:09 +0100

    Update yes-script.js

--- yes-script.js
+++ yes-script.js
@@ -3,12 +3,20 @@ let musicPlaying = false
 window.addEventListener('load', () => {
     launchConfetti()
 
-    // Autoplay music (works since user clicked Yes to get here)
     const music = document.getElementById('bg-music')
     music.volume = 0.3
-    music.play().catch(() => {})
-    musicPlaying = true
-    document.getElementById('music-toggle').textContent = '🔊'
+
+    music.play().then(() => {
+        musicPlaying = true
+        document.getElementById('music-toggle').textContent = '🔊'
+    }).catch(() => {
+        document.addEventListener('click', () => {
+            music.play().then(() => {
+                musicPlaying = true
+                document.getElementById('music-toggle').textContent = '🔊'
+            }).catch(() => {})
+        }, { once: true })
+    })
 })
 
 function launchConfetti() {
